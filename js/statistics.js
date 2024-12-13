8d82165f9efdb89ff3588ba657555da87f1afc61
gitonlinecloud <githubonline@outlook.com>
2024-12-13 11:50:11 +0800

    Update statistics.js

--- js/statistics.js
+++ js/statistics.js
@@ -1,7 +1,7 @@
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?b4126daaae1dc3ff710787203aaf41ad";
+  hm.src = "https://hm.baidu.com/hm.js?80a4209849e7f9537af466fdf0af08e4";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
